8f30dfa47f8b4fa043dfde3e564177c189dc6795
Pranay Kamble <iampranaykamble1@gmail.com>
2026-02-15 19:00:54 +0530

    feat(auth): add jwt functionality and testing

--- services/auth/internal/utils/utils_test.go
+++ services/auth/internal/utils/utils_test.go
@@ -24,3 +24,48 @@ func TestHashPassword(t *testing.T) {
 		assert.True(t, isValid, "Password should verify successfully: %s", plainPassword)
 	}
 }
+
+func TestInitKeys(t *testing.T) {
+
+	err := InitKeys()
+
+	assert.NoError(t, err, "InitKeys failed: check if the .pem files are in the right folder")
+}
+
+func TestJWTWorkflow(t *testing.T) {
+	err := InitKeys()
+	assert.NoError(t, err)
+
+	testID := "nano-12345"
+	testEmail := "test@example.com"
+	testRole := "admin"
+
+	tokenString, err := GetJWT(testID, testEmail, testRole)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, tokenString, "Token should not be empty")
+
+	claims, err := VerifyJWT(tokenString)
+
+	assert.NoError(t, err, "Valid token should not throw an error")
+	assert.NotNil(t, claims, "Claims should be returned")
+
+	assert.Equal(t, testID, claims["id"])
+	assert.Equal(t, testEmail, claims["email"])
+	assert.Equal(t, testRole, claims["role"])
+}
+
+func TestVerifyJWTHacker(t *testing.T) {
+	err := InitKeys()
+	assert.NoError(t, err)
+
+	fakeToken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.fake.signature"
+	_, err = VerifyJWT(fakeToken)
+	assert.Error(t, err, "A fake token MUST throw an error")
+
+	validToken, _ := GetJWT("123", "hacker@evil.com", "user")
+	tamperedToken := validToken[:len(validToken)-5] + "XXXXX"
+
+	_, err = VerifyJWT(tamperedToken)
+	assert.Error(t, err, "A tampered signature MUST be rejected")
+}
