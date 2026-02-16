7c2be3179d7f0c51bc74b5684229702651610550
Pranay Kamble <iampranaykamble1@gmail.com>
2026-02-16 17:26:05 +0530

    feat(auth): modify Register function in AuthService

--- services/auth/internal/service/auth_service.go
+++ services/auth/internal/service/auth_service.go
@@ -10,7 +10,7 @@ import (
 )
 
 type AuthService interface {
-	Register(ctx context.Context, name, email, password, role, provider, providerId string) error
+	Register(ctx context.Context, name, email, password, role, provider, providerId string) (*domain.User, error)
 }
 
 type authService struct {
@@ -21,30 +21,30 @@ func NewAuthService(repo repository.UserRepository) AuthService {
 	return &authService{repo: repo}
 }
 
-func (a *authService) Register(ctx context.Context, name, email, password, role, provider, providerId string) error {
+func (a *authService) Register(ctx context.Context, name, email, password, role, provider, providerId string) (*domain.User, error) {
 	res, err := a.repo.GetUserByEmail(ctx, email)
 
 	if err != nil {
-		return fmt.Errorf("service: failed to check email existence: %w", err)
+		return nil, fmt.Errorf("service: failed to check email existence: %w", err)
 	}
 
 	if res != nil {
-		return errors.New("service: email already exists")
+		return nil, errors.New("service: email already exists")
 	}
 
 	var hashedPassword string
 	hashedPassword, err = utils.HashPassword(password)
 
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	user := domain.User{Name: name, Email: email, Password: hashedPassword, Role: role, Provider: provider, ProviderID: providerId}
 
 	err = a.repo.CreateUser(ctx, &user)
 	if err != nil {
-		return fmt.Errorf("service: failed to create user: %w", err)
+		return nil, fmt.Errorf("service: failed to create user: %w", err)
 	}
 
-	return nil
+	return &user, nil
 }
