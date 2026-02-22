592fb581be7264fed68909056ee68cbf2edd7fd7
Pranay Kamble <iampranaykamble1@gmail.com>
2026-02-22 12:54:07 +0530

    refactor(auth-email): update pkg/database to decouple database connection layer

--- pkg/database/postgres.go
+++ pkg/database/postgres.go
@@ -9,22 +9,53 @@ import (
 	"gorm.io/gorm"
 )
 
-func Connect(dsn string) (*gorm.DB, error) {
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+type Postgres struct {
+	DB *gorm.DB
+}
 
+func NewPostgres(dsn string) (*Postgres, error) {
+	db, err := gorm.Open(postgres.Open(dsn))
 	if err != nil {
-		return nil, fmt.Errorf("failed to connect database: %w", err)
+		return nil, err
 	}
-	sqlDB, err := db.DB()
 
+	return &Postgres{DB: db}, nil
+}
+
+func (p *Postgres) Connect(dsn string) error {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		return nil, fmt.Errorf("failed to get underlying database object: %w", err)
+		return fmt.Errorf("database (postgres): failed to connect database: %w", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return fmt.Errorf("database (postgres): failed to get underlying database object: %w", err)
 	}
 
 	sqlDB.SetMaxIdleConns(10)
 	sqlDB.SetMaxOpenConns(100)
 	sqlDB.SetConnMaxLifetime(time.Hour)
 
-	logger.Info("Connected to database")
-	return db, nil
+	p.DB = db
+
+	logger.Info("Connected to PostgreSQL")
+	return nil
+}
+
+func (p *Postgres) Close() error {
+	if p.DB == nil {
+		return nil
+	}
+
+	sqlDb, err := p.DB.DB()
+	if err != nil {
+		return fmt.Errorf("database (postgres): failed to get underlying database object: %w", err)
+	}
+
+	err = sqlDb.Close()
+	if err != nil {
+		return fmt.Errorf("database (postgres): failed to close database: %w", err)
+	}
+	return nil
 }
