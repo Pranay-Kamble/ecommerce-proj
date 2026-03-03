8493eab9307ee40523b5044d5fdabc266a7c1409
Pranay Kamble <iampranaykamble1@gmail.com>
2026-03-03 12:11:58 +0530

    fix(catalog): add create functionality in ProductVariant repository, import updateinventory implementation to avoid race conditions

--- services/catalog/internal/repository/productvariant_repository.go
+++ services/catalog/internal/repository/productvariant_repository.go
@@ -10,8 +10,10 @@ import (
 )
 
 type ProductVariantRepository interface {
+	Create(ctx context.Context, item *domain.ProductVariant) error
 	GetBySKU(ctx context.Context, sku string) (*domain.ProductVariant, error)
 	UpdateInventory(ctx context.Context, sku string, quantityChange int) error
+	UpdatePrice(ctx context.Context, sku string, newPrice float64) error
 }
 
 type productVariantRepository struct {
@@ -20,6 +22,14 @@ type productVariantRepository struct {
 
 func NewProductVariantRepository(db *gorm.DB) ProductVariantRepository {
 	return &productVariantRepository{db: db}
+}
+
+func (pv *productVariantRepository) Create(ctx context.Context, item *domain.ProductVariant) error {
+	err := gorm.G[domain.ProductVariant](pv.db).Create(ctx, item)
+	if err != nil {
+		return fmt.Errorf("repository: failed to create product variant: %w", err)
+	}
+	return nil
 }
 
 func (pv *productVariantRepository) GetBySKU(ctx context.Context, sku string) (*domain.ProductVariant, error) {
@@ -34,10 +44,28 @@ func (pv *productVariantRepository) GetBySKU(ctx context.Context, sku string) (*
 }
 
 func (pv *productVariantRepository) UpdateInventory(ctx context.Context, sku string, quantityChange int) error {
-	_, err := gorm.G[*domain.ProductVariant](pv.db).Where("sku = ?", sku).Update(ctx, "inventory", gorm.Expr("inventory + ?", quantityChange))
+	rowsAffected, err := gorm.G[*domain.ProductVariant](pv.db).
+		Where("sku = ? AND inventory + ? >= 0", sku, quantityChange).
+		Update(ctx, "inventory", gorm.Expr("inventory + ?", quantityChange))
+
 	if err != nil {
-		return fmt.Errorf("repository: failed to update product variant quantity: %w", err)
+		return fmt.Errorf("repository: failed to update inventory: %w", err)
 	}
 
+	if rowsAffected == 0 {
+		return fmt.Errorf("repository: insufficient stock or invalid SKU")
+	}
+
+	return nil
+}
+
+func (pv *productVariantRepository) UpdatePrice(ctx context.Context, sku string, newPrice float64) error {
+	_, err := gorm.G[*domain.ProductVariant](pv.db).
+		Where("sku = ?", sku).
+		Update(ctx, "price", newPrice) // Just update the column directly
+
+	if err != nil {
+		return fmt.Errorf("repository: failed to update price: %w", err)
+	}
 	return nil
 }
