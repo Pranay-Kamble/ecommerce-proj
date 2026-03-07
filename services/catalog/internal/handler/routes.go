edfee75f8d265c3973bb7ed56ef896622cfc7a59
Pranay Kamble <iampranaykamble1@gmail.com>
2026-03-07 20:22:52 +0530

    chore(auth): log public key during retrieval

--- services/catalog/internal/handler/routes.go
+++ services/catalog/internal/handler/routes.go
@@ -1,5 +1,53 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"ecommerce/services/catalog/internal/service"
+	"net/http"
 
-func RegisterRoutes(r *gin.Engine) {}
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(
+	router *gin.Engine,
+	categoryHandler *CategoryHandler,
+	productHandler *ProductHandler,
+	sellerHandler *SellerHandler,
+	variantHandler *VariantHandler,
+	sellerService service.SellerService,
+) {
+	v1 := router.Group("/api/v1/catalog")
+
+	public := v1.Group("/")
+	{
+
+		public.GET("/categories", categoryHandler.GetAllCategories)
+		public.GET("/categories/:id/breadcrumbs", categoryHandler.GetBreadcrumbs)
+
+		public.GET("/products", productHandler.ListProducts)
+		public.GET("/products/:id", productHandler.GetProductByID)
+
+		public.GET("/variants/:sku", variantHandler.GetVariantByID)
+		public.GET("/ping", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"message": "pong"})
+		})
+	}
+
+	protected := v1.Group("/")
+	protected.Use(RequireUser)
+	{
+		protected.POST("/sellers", sellerHandler.CreateSeller)
+		protected.GET("/sellers/me", sellerHandler.GetMyProfile)
+	}
+
+	sellerRoutes := v1.Group("/seller")
+	sellerRoutes.Use(RequireUser, RequireSeller(sellerService))
+	{
+		sellerRoutes.POST("/products", productHandler.CreateProduct)
+		sellerRoutes.PUT("/products/:id", productHandler.UpdateProduct)
+		sellerRoutes.DELETE("/products/:id", productHandler.DeleteProduct)
+
+		sellerRoutes.POST("/products/:product_id/variants", variantHandler.CreateVariant)
+		sellerRoutes.PUT("/variants/:id", variantHandler.UpdateVariant)
+		sellerRoutes.DELETE("/variants/:id", variantHandler.DeleteVariant)
+	}
+}
