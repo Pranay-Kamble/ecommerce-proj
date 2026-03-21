cc596e72774f783b850d6542ae582c5a07212e6c
Pranay Kamble <iampranaykamble1@gmail.com>
2026-03-21 20:32:45 +0530

    feat(payment): added before create hook, and payment repository layer

--- services/payment/internal/domain/payment.go
+++ services/payment/internal/domain/payment.go
@@ -1,9 +1,11 @@
 package domain
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/sixafter/nanoid"
 	"gorm.io/gorm"
 )
 
@@ -22,4 +24,23 @@ type Payment struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+func (r *Payment) BeforeCreate(tx *gorm.DB) error {
+	if r.ID == uuid.Nil {
+		newID, err := uuid.NewV7()
+		if err != nil {
+			return fmt.Errorf("domain: could not generate payment ID: %w", err)
+		}
+		r.ID = newID
+	}
+	if r.PublicID == "" {
+		publicID, err := nanoid.New()
+		if err != nil {
+			return fmt.Errorf("domain: could not generate payment public ID: %w", err)
+		}
+		r.PublicID = publicID.String()
+	}
+
+	return nil
 }
