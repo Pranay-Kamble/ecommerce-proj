978f8f598f6756d463353ab9dfc591b308c6c7df
Pranay Kamble <iampranaykamble1@gmail.com>
2026-03-24 22:07:43 +0530

    feat(payment): start implementing payment service layer

--- services/payment/internal/service/payment_service.go
+++ services/payment/internal/service/payment_service.go
@@ -3,8 +3,10 @@ package service
 import (
 	"context"
 	"ecommerce/services/payment/internal/repository"
+	"fmt"
 
 	"github.com/stripe/stripe-go/v78"
+	"github.com/stripe/stripe-go/v78/checkout/session"
 )
 
 type PaymentService interface {
@@ -21,5 +23,34 @@ func NewPaymentService(repo repository.PaymentRepository, stripeSecretKey string
 }
 
 func (s *paymentService) CreateCheckoutSession(ctx context.Context, orderID string, userID string, amount int64, currency string) (string, string, error) {
-	panic("implement me")
+
+	params := &stripe.CheckoutSessionParams{
+		PaymentMethodTypes: stripe.StringSlice([]string{"card"}),
+		Mode:               stripe.String(string(stripe.CheckoutSessionModePayment)),
+		SuccessURL:         stripe.String("http://localhost:8085/success?session_id={CHECKOUT_SESSION_ID}"),
+		CancelURL:          stripe.String("http://localhost:8085/cancel"),
+		ClientReferenceID:  stripe.String(orderID),
+
+		LineItems: []*stripe.CheckoutSessionLineItemParams{
+			{
+				PriceData: &stripe.CheckoutSessionLineItemPriceDataParams{
+					Currency: stripe.String(currency),
+					ProductData: &stripe.CheckoutSessionLineItemPriceDataProductDataParams{
+						Name: stripe.String("Order #" + orderID),
+					},
+					UnitAmount: stripe.Int64(amount), // Remember: This is PAISE (300000 = ₹3000)
+				},
+				Quantity: stripe.Int64(1),
+			},
+		},
+	}
+
+	sess, err := session.New(params)
+	if err != nil {
+		return "", "", fmt.Errorf("service: failed to create checkout session: %w", err)
+	}
+
+	// TODO : Save this to Postgres database using s.paymentRepository
+
+	return sess.ID, sess.URL, nil
 }
